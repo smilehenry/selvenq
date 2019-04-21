c37b69a1f99ba2d38b772e12616bb60eb40397f0
zhanqi <970895140@qq.com>
2019-04-21 22:42:05 +0800

    add zuul

---
+++ server-zuul/src/main/java/com/sevlenq/serverzuul/ServerZuulApplication.java
@@ -0,0 +1,13 @@
+package com.sevlenq.serverzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerZuulApplication.class, args);
+    }
+
+}
